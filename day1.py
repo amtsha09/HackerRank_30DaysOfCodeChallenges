2ff53b4e255e3b40ae1638896a09ad93f2bd2b3c
Amit Sharma <asharm36@hawk.iit.edu>
2017-03-08 05:26:30 -0600

    day1.py

---
+++ day1.py
@@ -0,0 +1,29 @@
+####################### Day 1 ###########################
+
+if __name__=="__main__":
+    
+    i = 4
+    d = 4.0
+    s = 'HackerRank '
+    
+    # Declare second integer, double, and String variables.
+    integer = int()
+    doub = float()
+    string = str()
+    
+    # Read and save an integer, double, and String to your variables.
+    integer = int(input())
+    doub = float(input())
+    string = input()
+    
+    # Print the sum of both integer variables on a new line.
+    print(integer+i)
+    
+    # Print the sum of the double variables on a new line.
+    print(d+doub)
+    
+    # Concatenate and print the String variables on a new line
+    c = s+string
+    
+    # The 's' variable above should be printed first.
+    print(c)
