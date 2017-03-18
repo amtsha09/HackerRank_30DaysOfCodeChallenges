b0c4c0184fe0fb498dd89e0b0142f460f1eb65fb
Amit Sharma <asharm36@hawk.iit.edu>
2017-03-18 00:52:04 -0500

    day28.py

---
+++ day28.py
@@ -0,0 +1,14 @@
+############# Day 28 #################
+
+import sys
+import re
+
+if __name__ == "__main__"
+    N = int(input().strip())
+    names = []
+    for a0 in range(N):
+        firstName,emailID = input().strip().split(' ')
+        firstName,emailID = [str(firstName),str(emailID)]
+        if re.search('@gmail\.com$',emailID):
+            names.append(firstName)
+    print(*sorted(names),sep='\n')
